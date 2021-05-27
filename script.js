0611098361b2443d5b0804f83ced4c5360bf48cd
StepanR <pesiled@yandex.ru>
2021-05-27 23:36:08 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let money = 0;
+let income = 0;
+let addExpenses = 0;
+const deposit = 0; 
+const ission  = 0;
+const period = 0;
+
+// Переменные
+alert('Привет, JavaScript!');
+// Модальное окно 
+
+console.log('Мне нравится JavaScript! Он такой прикольный!');
+//Вывод текста в консоль
